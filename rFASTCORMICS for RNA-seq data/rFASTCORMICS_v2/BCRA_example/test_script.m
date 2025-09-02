9ed356dd314b5af3662f9486b81e23603362f6ba
Vanille LEJAL <vanille.lejal@uni.lu>
2025-09-02 17:29:40 +0200

    Adding an option for filling a missing medium

--- rFASTCORMICS for RNA-seq data/rFASTCORMICS_v2/BCRA_example/test_script.m
+++ rFASTCORMICS for RNA-seq data/rFASTCORMICS_v2/BCRA_example/test_script.m
@@ -8,9 +8,9 @@ changeCobraSolver('gurobi');
 load workspace_example.mat
 
 %%  Checking consistency of the original model
-A = fastcc(model, 1e-4, 1);
+consistentRxnsBool = fastcc(model, 1e-4, 1);
 
-consistent_model = removeRxns(model, model.rxns(setdiff(1:numel(model.rxns), A))); % create a consistent model based on the vector A
+consistent_model = removeRxns(model, model.rxns(setdiff(1:numel(model.rxns), consistentRxnsBool))); % create a consistent model based on the vector A
 fastcc(consistent_model, 1e-4, 1);
 
 consistent_model = changeObjective(consistent_model, "biomass_reaction");
@@ -42,3 +42,13 @@ fba_BCRA = optimizeCbModel(BCRAmodel, 'max', 'zero');
 % For the context-specific model:
 [grRatio_BCRA, grRateKO_BCRA, ...
     grRateWT_BCRA, ~, ~, geneList_BCRA] = singleGeneDeletion(BCRAmodel, 'FBA', [], 0, 1);
+
+%% Example with a non-sufficient medium
+optionalSettingsBis = optionalSettings;
+to_remove = {'glc_D[e]', 'o2[e]', 'ala_L[e]', 'arg_L[e]', 'asn_L[e]', 'asp_L[e]', 'Lcystin[e]', 'glu_L[e]', 'gly[e]', 'his_L[e]', 'ile_L[e]', 'leu_L[e]', 'lys_L[e]', 'met_L[e]', 'phe_L[e]', 'cys_L[e]', 'ser_L[e]', 'thr_L[e]'};
+mask = ~ismember(optionalSettingsBis.medium, to_remove);
+optionalSettingsBis.medium = optionalSettingsBis.medium(mask);
+
+[BCRAmodelBis, retainedRxnsBis] = rFastcormics4cobra_v2(consistent_model, discretized, rownames, dico, ...
+    consensusProportion, epsilon, optionalSettingsBis, biomassRxn); %filling the medium is activated by default
+
--- rFASTCORMICS for RNA-seq data/rFASTCORMICS_v2/scripts/rFASTCORMICS/rFastcormics4cobra_v2.m
+++ rFASTCORMICS for RNA-seq data/rFASTCORMICS_v2/scripts/rFASTCORMICS/rFastcormics4cobra_v2.m
@@ -1,4 +1,4 @@
-function [contextSpecificModel, Afinal] = rFastcormics4cobra_v2(model, discretized, rownames, dico, consensusProportion, epsilon, optionalSettings, biomassReactionName, adaptiveScalingFlag)
+function [contextSpecificModel, retainedRxns] = rFastcormics4cobra_v2(model, discretized, rownames, dico, consensusProportion, epsilon, optionalSettings, biomassReactionName, fillingMediumFlag, adaptiveScalingFlag)
 % The rFASTCORMICS is a context-specific building algorithm for
 % reconstructing a tissue, a cell-specific, or any context-specific model from RNAseq data
 % and a generic reconstruction (Pacheco et al. 2019)
@@ -33,7 +33,8 @@ function [contextSpecificModel, Afinal] = rFastcormics4cobra_v2(model, discretiz
 %                        * medium - cell array of metabolites abbreviation that defines metabolites
 %                          in the growth medium of cells to constrain the model, see example medium_example.mat
 %                        * notMediumConstrained - react ????
-% biomassReactionName:   cell array or all other functions that might require some exchange reactions to ??? (default '')
+% biomassReactionName:   cell array or all other functions that might require some exchange reactions to ??? (default '')/obj function ??
+% fillingMediumFlag      filling the medium in case it's not sufficient for the model to fulfill the objective function. 1 for active (default), 0 for inactive
 % adaptiveScalingFlag:   0 for inactive (default), 1 for active
 
 % OUTPUT:
@@ -48,11 +49,14 @@ function [contextSpecificModel, Afinal] = rFastcormics4cobra_v2(model, discretiz
 %       of the code
 
 %% Initializing the arguments
-if nargin < 9
+if nargin < 10
     adaptiveScalingFlag = 0;
 end
+if nargin < 9
+    fillingMediumFlag = 1;
+end
 if nargin < 8
-    biomassReactionName = [];
+    biomassReactionName = 'biomass_reaction';
 end
 if nargin < 7
     optionalSettings = [];
@@ -69,18 +73,22 @@ if isfield(optionalSettings, 'func')
 else
     functionKeep = '';
 end
+
 %% Saving the original model
 origModel = model;
+
 %% Checking required solver and tooboxes
 % check if it works if one solver/toolbox is missing
 requiredSolvers = {'ibm_cplex', 'gurobi'};
 requiredToolboxes = {'curve_fitting_toolbox', 'optimization_toolbox'};
 solversPkgs = prepareTest('requireOneSolverOf', requiredSolvers, 'requiredToolboxes', requiredToolboxes);
+
 %% Checking the format of the discretized values
 if numel(rownames) ~= size(discretized, 1)
     disp('The number of gene IDs between the discretized data and the given IDs ("rownames") do not correspond')
     return
 end
+
 %% Correcting the format of the model
 
 % checking model.subSystems format 
@@ -98,8 +106,9 @@ end
 model = fixIrrRFASTCORMICS(model); %need to check that
 
 % creating a consistent model
-[A, ~, ~] = fastcc(model, epsilon, 0, 0, 'original'); %indexes of the consistent reactions
-consistentModel = removeRxns(model, model.rxns(setdiff(1:numel(model.rxns), A))); %removing the non consistent reactions
+[consistentRxns, ~, ~] = fastcc(model, epsilon, 0, 0, 'original'); %indexes of the consistent reactions
+consistentModel = removeRxns(model, model.rxns(setdiff(1:numel(model.rxns), consistentRxns))); %removing the non consistent reactions
+
 %% Optionally the model can be constrained in function of the medium
 if ~isempty(optionalSettings) && isfield(optionalSettings, 'medium')
     %check if the medium constrain works
@@ -109,11 +118,14 @@ if ~isempty(optionalSettings) && isfield(optionalSettings, 'medium')
         notMediumConstrained = [];
     end
     mediumMets = optionalSettings.medium;
+    % finding the exchange reactions associated with the medium
+    [excRxnsBool, ~] = findExcRxns(consistentModel);
+    excRxns = consistentModel.rxns(excRxnsBool);
+    [mediumRxns, ~] = findRxnsFromMets(consistentModel, mediumMets);
+    excMediumRxns = intersect(excRxns, mediumRxns);
 
     mediumConstrainedModel =  constrainModelRFASTCORMICS(consistentModel, mediumMets, notMediumConstrained, biomassReactionName, functionKeep);
     % watch out, the model here could be unconsistent
-    
-    % fillingMedium shoud be added here
     
 elseif isempty(optionalSettings)
     mediumConstrainedModel = consistentModel; 
@@ -122,6 +134,7 @@ else
     mediumConstrainedModel = consistentModel;
     warning('No given medium')
 end
+
 %% Mapping the reactions to the model
 mapping = mapExpressionToModel(mediumConstrainedModel, discretized, dico, rownames, 1);
 mapping = sparse(mapping);
@@ -167,15 +180,76 @@ mediumConstrainedModel.lb(notExpressed) = 0;
 mediumConstrainedModel.ub(notExpressed) = 0;
 
 % Building of a consistent model
-[A, ~, ~] = fastcc(mediumConstrainedModel, epsilon, 0, 0, 'original');
-consistentMediumConstrainedModel = removeRxns(mediumConstrainedModel, mediumConstrainedModel.rxns(setdiff(1:numel(mediumConstrainedModel.rxns), A)));
+[consistentRxnsAfterMedium, ~, ~] = fastcc(mediumConstrainedModel, epsilon, 0, 0, 'original');
+consistentMediumConstrainedModel = removeRxns(mediumConstrainedModel, mediumConstrainedModel.rxns(setdiff(1:numel(mediumConstrainedModel.rxns), consistentRxnsAfterMedium)));
 
-%Get the indices of the core reactions in the consistent constrained model
+%% Checking medium sufficiency + add this loop after fastcore also
+if any(strcmp(consistentMediumConstrainedModel.rxns, biomassReactionName))
+    consistentMediumConstrainedModel = changeObjective(consistentMediumConstrainedModel, biomassReactionName);
+    fbaResults = optimizeCbModel(consistentMediumConstrainedModel, 'max', 'zero');
+
+    if ~isempty(fbaResults.f) && ~isnan(fbaResults.f)
+        disp(['The model still contains ' biomassReactionName ' after the application of medium constraints, and FBA result is not null.' newline 'Medium is sufficient. Continuing with fastcore.']);
+        needMediumFilling = false;
+    else
+        disp(['The model still contains ' biomassReactionName ' after the application of medium constraints, but FBA result is ' num2str(fbaResults.f) '. Medium is not sufficient.']);
+        needMediumFilling = true;
+    end
+else
+    disp(['The model lost ' biomassReactionName ' after the application of medium constraints.']);
+    needMediumFilling = true;
+end
+
+%% In case medium if not sufficient
+%Checking if all the exc rxns associated with the medium are in. If not, force them to be in the core
+if needMediumFilling
+    [excRxnsAfterMediumBool, ~] = findExcRxns(consistentMediumConstrainedModel);
+    excRxnsAfterMedium = consistentMediumConstrainedModel.rxns(excRxnsAfterMediumBool);
+    notPresentExcMediumRxns = setdiff(excMediumRxns, excRxnsAfterMedium);
+    if ~isempty(notPresentExcMediumRxns)
+        disp(['The following exchange reactions: ' newline strjoin(notPresentExcMediumRxns, newline) newline 'were initially not included in the consistent medium constraint model, even though ther were associated with the medium metabolites.' newline 'In the case of medium filling, they will be forced to be included in the model.']);
+    end
+end
+
+%% Get the indices of the core reactions in the consistent constrained model
 correctIndicesCompletedCore = find(ismember(consistentMediumConstrainedModel.rxns, rxnNamesCompletedCore));
 
 %% building of the context-specific model
-[contextSpecificModel, A2] = fastcore(consistentMediumConstrainedModel, correctIndicesCompletedCore, 1e-4, 0, adaptiveScalingFlag);
+[contextSpecificModel, retainedRxnsBool] = fastcore(consistentMediumConstrainedModel, correctIndicesCompletedCore, 1e-4, 0, adaptiveScalingFlag);
 indices = 1:numel(consistentMediumConstrainedModel.rxns);
-keepRxns = indices(A2 == 1);
+keepRxns = indices(retainedRxnsBool == 1);
+retainedRxns = find(ismember(origModel.rxns, consistentMediumConstrainedModel.rxns(keepRxns)));
 
-Afinal = find(ismember(origModel.rxns, consistentMediumConstrainedModel.rxns(keepRxns)));
+%% Proceed to medium filling if needed and required
+if fillingMediumFlag == 1 && needMediumFilling
+    disp('Proceeding to medium filling.');
+    
+    % copying the bounds of rxns in the context specific model in the consistent model to preserve the medium constraints
+    [~, idxConsistentModel, idxContextSpecificModel] = intersect(consistentModel.rxns, contextSpecificModel.rxns);
+    consistentModel.lb(idxConsistentModel) = contextSpecificModel.lb(idxContextSpecificModel);
+    consistentModel.ub(idxConsistentModel) = contextSpecificModel.ub(idxContextSpecificModel);
+    
+    % finding the exchange reactions associated with the medium
+    [excRxnsCtxtSpeModelBool, ~] = findExcRxns(contextSpecificModel);
+    excRxnsCtxtSpeModel = contextSpecificModel.rxns(excRxnsCtxtSpeModelBool);
+    notPresentExcMediumRxnsSpe = setdiff(excMediumRxns, excRxnsCtxtSpeModel);
+    disp(['The following exchange reactions: ' newline strjoin(notPresentExcMediumRxnsSpe, newline) newline 'will first be used to fill the medium the model as they are initially associated with the medium metabolites.']);
+    
+    % creating a new core with all the rxns of the contextSpecificModel,
+    % the biomass, the .func rxns, and the missing medium exchange rxns
+    mediumFillingCore = unique([contextSpecificModel.rxns; biomassReactionName; functionKeep; notPresentExcMediumRxnsSpe]);
+    indicesMediumFillingCore = find(ismember(consistentModel.rxns, mediumFillingCore));
+    
+    % completing the medium and the model 
+    [contextSpecificModel, retainedRxnsMediumFilledBool] = fastcore(consistentModel, indicesMediumFillingCore, 1e-4, 0, adaptiveScalingFlag);
+    indices = 1:numel(consistentModel.rxns);
+    keepRxns = indices(retainedRxnsMediumFilledBool == 1);
+    retainedRxnsMediumFilling = find(ismember(origModel.rxns, consistentModel.rxns(keepRxns)));
+    supplementaryRxns = setdiff(retainedRxnsMediumFilling, retainedRxns);
+    disp(['Here are the final reactions that have been added to the model in order to fill the medium: ' newline strjoin(origModel.rxns(supplementaryRxns), newline) newline]);
+    retainedRxns = retainedRxnsMediumFilling;
+end
+
+%%
+disp('rFastcormics is done.');    
+end
