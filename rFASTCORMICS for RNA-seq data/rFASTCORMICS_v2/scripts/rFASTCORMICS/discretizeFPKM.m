61cd9ebd45bf3bf5b56a1d1a3137daf7c4fb99b7
Leonie THOMAS <leonie.thomas@uni.lu>
2026-04-22 16:24:27 +0200

    enable the discretizeFPKM function to detect when the expression data is left skewed and adjust accordingly

--- rFASTCORMICS for RNA-seq data/rFASTCORMICS_v2/scripts/rFASTCORMICS/discretizeFPKM.m
+++ rFASTCORMICS for RNA-seq data/rFASTCORMICS_v2/scripts/rFASTCORMICS/discretizeFPKM.m
@@ -1,4 +1,4 @@
-function [discretized, scaledExpression] = discretizeFPKM(fpkm, colnames, figflag, pathFigures)
+function [discretized, scaledExpression] = discretizeFPKM(fpkm, colnames, figflag, pathFigures,fileFormat)
 %   The function discretizes gene expression data (FPKM values) into three categories:
 %   expressed, not expressed, and unknown, based on a zFPKM transformation and
 %   half-Gaussian density fitting.
@@ -12,6 +12,7 @@ function [discretized, scaledExpression] = discretizeFPKM(fpkm, colnames, figfla
 %   colnames:          1 x n cell array of sample names
 %   figflag:           optional, flag to plot and save figures (default: 0)
 %   pathFigures:       optional, path to save figures (default: current folder)
+%   fileFormat:        optional, file format you want to store the figure as
 %
 % OUTPUTS:
 %   discretized:       m x n matrix of discretized values
@@ -32,6 +33,10 @@ end
 if nargin < 4
     pathFigures = '';
 end
+if nargin < 5
+    fileFormat = '.png';
+end
+
 
 %% Convert table input to array if needed
 if istable(fpkm)
@@ -68,9 +73,9 @@ for j = 1:size(fpkm, 2) % for each sample
         xlabel('log2(FPKM)');
         title({'Histogram of abundance', ['Sample: ', colnames{j}]}, 'Interpreter', 'none');
         if strcmp(pathFigures, '')
-            saveas(gcf, ['Figures\Discretization\histogram_', colnames{j}, '.png'])
+            saveas(gcf, ['Figures\Discretization\histogram_', colnames{j}, fileFormat])
         else
-            saveas(gcf,[pathFigures,'Figures\Discretization\histogram_', colnames{j},'.png'])
+            saveas(gcf,[pathFigures,'Figures\Discretization\histogram_', colnames{j},fileFormat])
         end
     end
     
@@ -83,61 +88,104 @@ for j = 1:size(fpkm, 2) % for each sample
         ylabel('Density'); xlabel('log2(FPKM)');
         title({'Density plot', ['Sample: ', colnames{j}]}, 'Interpreter', 'none');
         legend({'Signal'}, 'Location', 'best')
-        saveas(gcf, [pathFigures, 'Figures\Discretization\density_', colnames{j}, '.png']);
+        saveas(gcf, [pathFigures, 'Figures\Discretization\density_', colnames{j}, fileFormat]);
     end
 
-    %% create right curve
-    peak_max_idx = (find(probability_estimate == max(probability_estimate))); % find the maximum of the density curve
-    max_r_side = probability_estimate(peak_max_idx:end); % copy right-most side of density curve
-    max_l_side = flip(max_r_side); % mirror the right side
-    hybrid = [max_l_side(1:end-1), max_r_side];
-    
-    % Adjust hybrid curve to match length of density
-    hybrid_curve = zeros(numel(probability_estimate), 1);
-    if numel(hybrid) > numel(probability_estimate)
-        hybrid_curve(end+1-numel(hybrid(end-100+1:end)):end) = hybrid(end-100+1:end);
-        % x = zeros(numel(probability_estimate),1); % create new x-axis
+    %% =========================
+    %% METHOD 1 (RIGHT-SIDE PEAK)
+    peak_idx_r = find(probability_estimate == max(probability_estimate(50:100)));
+
+    max_r_side = probability_estimate(peak_idx_r:end);
+    max_l_side = flip(max_r_side);
+    hybrid_r = [max_l_side(1:end-1), max_r_side];
+
+    hybrid_curve_r = zeros(numel(probability_estimate),1);
+    if numel(hybrid_r)> numel(probability_estimate)
+        hybrid_curve_r(end+1-numel(hybrid_r(end-100+1:end)):end)=hybrid_r(end-100+1:end);
         x = xi;
     else
-        hybrid_curve(end-numel(hybrid)+1:end) = hybrid;
+        hybrid_curve_r(end-numel(hybrid_r)+1:end) = hybrid_r;
         x = xi;
     end
+
+    rest_curve_r = probability_estimate - hybrid_curve_r';
+    rest_curve_r(rest_curve_r<0.0001)=0;
     
     if figflag
-        plot(x, hybrid_curve, 'b--', 'LineWidth', 2);
+        plot(x, hybrid_curve_r, 'b--', 'LineWidth', 2);
         legend({'Signal','hybrid curve right'}, 'Location', 'best')
         title({'Density plot with hybrid curves', ['Sample: ', colnames{j}]}, 'Interpreter', 'none');
     end
     
-    %% create left curve (main curve - rightmost curve)
-    rest_curve = probability_estimate - hybrid_curve';
-    rest_curve(rest_curve < 0.0001) = 0;
-    
     if figflag
-        plot(xi, rest_curve, 'r--', 'LineWidth', 2);
+        plot(xi, rest_curve_r, 'r--', 'LineWidth', 2);
         legend({'Signal', 'hybrid curve right', 'hybrid curve left'}, 'Location', 'best')
-        saveas(gcf, [pathFigures,'Figures\Discretization\density_maxfit_', colnames{j}, '.png']);
+        saveas(gcf, [pathFigures,'Figures\Discretization\density_maxfit_', colnames{j}, fileFormat]);
     end
 
     %% fit half-Gaussian curves to right and left curves
-    [fitresult_r, ~] = createFitR(xi, hybrid_curve);
-    [fitresult_l, ~] = createFitL(xi, rest_curve);
+    [fit_r_r, ~] = createFitR(xi, hybrid_curve_r);
+    [fit_l_r, ~] = createFitL(xi, rest_curve_r);
     
     if figflag
-        plot(fitresult_r, 'b');
-        plot(fitresult_l, 'g');
+        plot(fit_r_r, 'b');
+        plot(fit_l_r, 'g');
         title({'Density plot with fitted curves', ['Sample: ', colnames{j}]}, 'Interpreter','none');
         ylabel('Density');
         xlabel('log2(FPKM)');
         legend({'Signal', 'hybrid curve right', 'hybrid curve left', 'fitted curve right', 'fitted curve left'}, 'Location','best')
-        saveas(gcf, [pathFigures, 'Figures\Discretization\density_fitted_', colnames{j}, '.png']);
+        saveas(gcf, [pathFigures, 'Figures\Discretization\density_fitted_', colnames{j}, fileFormat]);
         close all;
     end
     
     %% zFPKM transform the data and plot the density
     
-    sigma1 = fitresult_r.c1/sqrt(2);
-    mu1 = fitresult_r.b1; % x-value of right max
+    sigma1_r = fit_r_r.c1/sqrt(2);
+    mu1_r = fit_r_r.b1;
+    mu2_r = fit_l_r.b1;
+    ue_r = max(-3,(mu2_r-mu1_r)/sigma1_r);
+
+    %% =========================
+    %% METHOD 2 (GLOBAL PEAK)
+    peak_idx_g = find(probability_estimate == max(probability_estimate));
+
+    max_r_side = probability_estimate(peak_idx_g:end);
+    max_l_side = flip(max_r_side);
+    hybrid_g = [max_l_side(1:end-1), max_r_side];
+
+    hybrid_curve_g = zeros(numel(probability_estimate),1);
+    if numel(hybrid_g)> numel(probability_estimate)
+        hybrid_curve_g(end+1-numel(hybrid_g(end-100+1:end)):end)=hybrid_g(end-100+1:end);
+    else
+        hybrid_curve_g(end-numel(hybrid_g)+1:end) = hybrid_g;
+    end
+
+    rest_curve_g = probability_estimate - hybrid_curve_g';
+    rest_curve_g(rest_curve_g<0.0001)=0;
+
+    [fit_r_g, ~] = createFit_r(xi, hybrid_curve_g);
+    [fit_l_g, ~] = createFit_l(xi, rest_curve_g);
+
+    sigma1_g = fit_r_g.c1/sqrt(2);
+    mu1_g = fit_r_g.b1;
+    mu2_g = fit_l_g.b1;
+    ue_g = max(-3,(mu2_g-mu1_g)/sigma1_g);
+
+    %% SELECT METHOD
+    if ue_r < 0
+        mu1 = mu1_r;
+        mu2 = mu2_r;
+        sigma1 = sigma1_r;
+        ue = ue_r;
+    else
+        mu1 = mu1_g;
+        mu2 = mu2_g;
+        sigma1 = sigma1_g;
+        ue = ue_g;
+    end
+
+
+
     
     zFPKM = (signal_sample - mu1)/sigma1;
     [yFPKM, xFPKM] = ksdensity(zFPKM);
@@ -145,9 +193,6 @@ for j = 1:size(fpkm, 2) % for each sample
     %% do the analysis
     
     zFPKM = (signal(:, j) - mu1)/sigma1; %transform sample into zFPKM
-    
-    mu2 = fitresult_l.b1;
-    mu2_t = (mu2-mu1)/sigma1; % x-value of left max
     
     if figflag
         plot([mu1, mu1], [0, max(probability_estimate)])
@@ -160,7 +205,6 @@ for j = 1:size(fpkm, 2) % for each sample
     discretized = zFPKM;
     
     e = 0;
-    ue = max(-3, mu2_t);
     
     if figflag
         figure;
@@ -173,8 +217,12 @@ for j = 1:size(fpkm, 2) % for each sample
         legend({'zFPKM data', 'expression threshold', 'inexpression threshold'}, 'Location', 'best')
         title({'Expression thresholds', ['Sample: ', colnames{j}], ['inexpression threshold = ', num2str(ue)]}, 'Interpreter', 'none');
         
-        saveas(gcf, [pathFigures, 'Figures\Discretization\zFPKM_', colnames{j}, '.png']);
+        saveas(gcf, [pathFigures, 'Figures\Discretization\zFPKM_', colnames{j}, fileFormat]);
         close all;
+    end
+
+    if ue_g > 0 
+        disp("Your distribution was left skewed!")
     end
     
     zFPKM = reshape(zFPKM, size(data_keep, 1), size(data_keep, 2));
@@ -217,10 +265,10 @@ if figflag
     xlabel('log2(FPKM)');
     % legend(colnames,'interpreter','none','Location','best')
     if strcmp(pathFigures, '')
-        saveas(gcf, 'Figures\Discretization\density_all.png'); 
+        saveas(gcf, ['Figures\Discretization\density_all', fileFormat]); 
         close all
     else
-        saveas(gcf,[pathFigures,'Figures\Discretization\density_all.png']); 
+        saveas(gcf,[pathFigures,'Figures\Discretization\density_all',fileFormat]); 
         close all
     end
 end
@@ -243,10 +291,10 @@ if figflag
         % xlabel('log2(FPKM)');
     end
     if strcmp(pathFigures, '')
-        saveas(gcf, 'Figures\Discretization\density_plots.png'); 
+        saveas(gcf, ['Figures\Discretization\density_plots',fileFormat]); 
         close all
     else
-        saveas(gcf, [pathFigures, 'Figures\Discretization\density_plots.png']); 
+        saveas(gcf, [pathFigures, 'Figures\Discretization\density_plots',fileFormat]); 
         close all
     end
 end
